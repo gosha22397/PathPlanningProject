e5ad4a728bd6b22131cb70a6979dc11b699ed9ab
gosha22397 <gosha22397@yandex.ru>
2019-12-24 05:07:16 +0300

    Add travis work

--- Build/Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gosha22397/Yandex.Disk-goshik.novikov22397.localized/Programmirovanie/PathPlanningProject
-# Build directory: /Users/gosha22397/Yandex.Disk-goshik.novikov22397.localized/Programmirovanie/PathPlanningProject/Build/Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Src")
-subdirs("Tests")
