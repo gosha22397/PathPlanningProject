dc7d0f817e6c255fb1c4e661d9994c33c8f65b97
gosha22397 <gosha22397@yandex.ru>
2020-01-13 01:41:59 +0300

    KT-1 ver_6 (Add tests and Build folder erase)

--- Build/Release/Src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gosha22397/Yandex.Disk-goshik.novikov22397.localized/Programmirovanie/PathPlanningProject/Src
-# Build directory: /Users/gosha22397/Yandex.Disk-goshik.novikov22397.localized/Programmirovanie/PathPlanningProject/Build/Release/Src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
